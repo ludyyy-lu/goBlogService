2391185c123ab5055ceba151f9db74eb3cd3779f
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-03-15 20:26:13 +0800

    类型转换 | 分页管理

---
+++ pkg/app/pagination.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ludyyy-lu/goBlogService/global"
+	"github.com/ludyyy-lu/goBlogService/pkg/convert"
+)
+
+func GetPage(c *gin.Context) int {
+	pageSize := convert.StrTo(c.Query("page")).MustInt()
+	if pageSize <= 0 {
+		return global.AppSetting.DefaultPageSize
+	}
+	if pageSize > global.AppSetting.MaxPageSize {
+		return global.AppSetting.MaxPageSize
+	}
+	return pageSize
+}
+
+func GetPageOffset(page, pageSize int) int {
+	result := 0
+	if page > 0 {
+		result = (page - 1) * pageSize
+	}
+	return result
+}
